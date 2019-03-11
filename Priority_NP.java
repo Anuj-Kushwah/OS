2d11ae509b9cbbdd3ba7eff99fc315c8360696bf
Anuj-Kushwah <kushwahanuj.ak@yahoo.com>
2019-03-11 23:13:08 +0530

    Update Priority_NP.java

--- Priority_NP.java
+++ Priority_NP.java
@@ -1,99 +1,83 @@
-public class Os {
+package os;
+public class prio {
+
     public static void main(String[] args) {
-        int[] P = {1,2,3,4};
-        int[] At = {1,0,2,3};
-        int[] Bt = {2,4,3,2};
-        int[] Pt = {1,2,1,3};
-        int[] Ct = new int[4];
-        int[] Tat = new int[4];
-        int[] Wt = new int[4];
+
+        int[] P = {1,2,3,4,5,6,7};
+
+        int[] At = {0,2,1,4,6,5,7};
+
+        int[] Bt = {3,5,4,2,9,4,10};
+
+        int[] Pt = {2,6,3,5,7,4,10};
+
+        int[] Ct = {0,0,0,0,0,0,0};
+
+        int[] Tat = new int[7];
+
+        int[] Wt = new int[7];
+
         float AvgWt=0, AvgTat=0;
-        for(int k=1;k<At.length;k++){
-        for(int i=1;i<At.length;i++){
-        if(At[i-1]>At[i]){
-            int temp = At[i-1];
-            At[i-1] = At[i];
-            At[i] = temp;
-            temp = P[i-1];
-            P[i-1] = P[i];
-            P[i] = temp;
-            temp = Bt[i-1];
-            Bt[i-1] = Bt[i];
-            Bt[i] = temp;
-            temp = Pt[i-1];
-            Pt[i-1] = Pt[i];
-            Pt[i] = temp;
-       }
+
+        int count = 0,CuT=0;
+        while(true){
+        if(count == At.length){
+            break;
         }
-       }
-        for(int k=1;k<At.length;k++){
-      for(int i=0;i<At.length-1;i++){
-            if(At[i]==At[i+1]&&Pt[i]>Pt[i+1]){
-               int temp = Bt[i];
-               Bt[i] = Bt[i+1];
-               Bt[i+1]= temp;
-               temp = At[i+1];
-               At[i+1] = At[i];
-                At[i] = temp;
-                temp = P[i+1];
-                P[i+1] = P[i];
-                P[i] = temp;
-                temp = Pt[i-1];
-                Pt[i-1] = Pt[i];
-                Pt[i] = temp;
+        int min =99 , index=At.length;
+        for(int i =0;i<At.length;i++){
             
-           }}
-           }
- 
-          for(int i=0;i<At.length;i++){
-              
-                if(i == 0){
-			Ct[i] =At[i]+ Bt[i];
-		}
-                else if(Ct[i-1]<At[i]){
-                       Ct[i]=(Bt[i]+ Ct[i-1])+(At[i]-Ct[i-1]);
-                }
-                else{
-                    	Ct[i]= Bt[i]+ Ct[i-1];
-		}
-	}
-          for(int k=1;k<At.length;k++){
-      for(int i=1;i<At.length;i++){
-        if(P[i]<P[i-1]){
-            int temp = At[i-1];
-            At[i-1] = At[i];
-            At[i] = temp;
-            temp = P[i-1];
-            P[i-1] = P[i];
-            P[i] = temp;
-            temp = Bt[i-1];
-            Bt[i-1] = Bt[i];
-            Bt[i] = temp;
-            temp = Ct[i-1];
-            Ct[i-1] = Ct[i];
-            Ct[i] = temp;
-             temp = Pt[i-1];
-                Pt[i-1] = Pt[i];
-                Pt[i] = temp;
+            if(At[i]<=CuT && Ct[i]==0 && Pt[i]<min){
+                min = Pt[i];
+                index = i;
+            }
         }
-      }
-          }
+        if(index==At.length){
+            CuT++;
+        }else{
+            CuT = CuT + Bt[index];
+            count++;
+            Ct[index] = CuT;
+        }
+        
+        }
+        
+        
+        
          for(int j=0;j<P.length;j++){
+
             Tat[j] = Ct[j] - At[j];
+
             Wt[j] = Tat[j] - Bt[j];
+
             AvgTat = AvgTat + Tat[j];
+
             AvgWt = AvgWt + Wt[j];
+
        }
+
         AvgTat = AvgTat/(P.length );
+
         AvgWt = AvgWt/(P.length );     
+
         System.out.println("P\tA\tB\tC\tT\tW\tPt");
+
       for(int j=0;j<At.length;j++){
+
             System.out.println(P[j]+"\t"+At[j]+"\t"+Bt[j]+"\t"+Ct[j]+"\t"+Tat[j]+"\t"+Wt[j]+"\t"+Pt[j]);
+
             
+
        }
+
         System.out.println();
+
        System.out.println("Average TAT \t"+AvgTat);
+
        System.out.println("Average WT \t"+AvgWt);
+
     }
+
     
+
 }
